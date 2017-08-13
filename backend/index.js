432e723f0c26750d01574b5931cc4a898b0201d3
glebpro <gleb.promokhov@gmail.com>
2017-08-13 14:55:27 -0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,35 @@
 const express = require('express')
-const cors = require('cors');
-const indexMethod = require("./methods/indexMethod");
+const bodyParser = require('body-parser');
+const indexMethod = require("./rest/methods/indexMethod");
 
-const app = express()
+const app = express();
 
-const corsOptions = {
-  preflightContinue: true,
+// the cors() was being dumb...
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
 };
 
-app.use(cors(corsOptions));
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
 
-app.use('/v1/', indexMethod);
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now());
+  next();
+});
 
-app.get('/v2/', function (req, res){
+app.use('/v1', indexMethod);
+
+app.get('/v2', function (req, res){
   res.send("Hello v2!");
 });
 
--- frontend/js/views/Home/index.jsx
+++ frontend/js/views/Home/index.jsx
@@ -16,7 +16,7 @@ export default class Home extends Component {
   }
 
   componentWillUnmount() {
-    this.serverRequest.abort();
+    // this.serverRequest.abort();
   }
 
   render() {
